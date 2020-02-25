62c0258ed14ab3d185e415420df02c01d2a88208
pythonpete32 <pythonpete32@gmail.com>
2020-02-25 03:45:20 +0000

    add Data Store

--- index.js
+++ index.js
@@ -3,30 +3,44 @@
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
-
+const Configstore = require('configstore');
 
 // lib imports
+const inquirer = require('./lib/inquirer');
 
-
+// data store
+const pkg = require('./package.json')
+const daoConf = new Configstore(pkg.name);
 
 // banner
 clear();
 console.log(
-    chalk.yellow(
-        figlet.textSync('G-Strap', {
+    chalk.blueBright(
+        figlet.textSync('Power Tools', {
             horizontalLayout: 'full'
         })
     )
-);
+)
+console.log(
+    chalk.blue.bold('For Aragon Power Users')
+)
 
 // check for saved dao
+if (true) { // no saved dao
+    console.log('No Saved DAO')
+}
 
 
 // run program
 const run = async () => {
     try {
-        console.log("running............")
-    } catch(err) {
+        const addresses = await inquirer.askDaoAddresses()
+        Object.keys(addresses).map(function (key, index) {
+            daoConf.set(key, addresses[key])
+        });
+
+        console.log(daoConf.get('dao'));
+    } catch (err) {
         console.log(err)
     }
 }
