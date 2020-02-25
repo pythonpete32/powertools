05a6d42a30c996a6112855331c67342ff551af41
pythonpete32 <pythonpete32@gmail.com>
2020-02-25 12:21:00 +0000

    add login flow

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 const Configstore = require('configstore');
+const chalkAnimation = require('chalk-animation');
+
 
 // lib imports
 const inquirer = require('./lib/inquirer');
@@ -13,34 +15,44 @@ const pkg = require('./package.json')
 const daoConf = new Configstore(pkg.name);
 
 // banner
-clear();
-console.log(
-    chalk.blueBright(
-        figlet.textSync('Power Tools', {
-            horizontalLayout: 'full'
-        })
+const banner = () => {
+    clear();
+    console.log(
+        chalk.blueBright(
+            figlet.textSync('Power Tools', {
+                horizontalLayout: 'full'
+            })
+        )
     )
-)
-console.log(
-    chalk.blue.bold('For Aragon Power Users')
-)
+
+    console.log(
+        chalk.blue.bold('For Aragon Power Users')
+    )
+}
 
 // check for saved dao
-if (true) { // no saved dao
-    console.log('No Saved DAO')
+if (daoConf) { // no saved dao
+    console.log(daoConf)
+    setTimeout(() => {
+    }, 1000);
 }
 
 
 // run program
 const run = async () => {
-    try {
+    banner()
 
+    try {
+        const newDAO = await inquirer.login()
+        console.log(newDAO)
+        if (newDAO){
+            await inquirer.askDaoAddresses()
+        }    
+        
         await inquirer.getMints()
-        await inquirer.askDaoAddresses()
 
     } catch (err) {
         console.log(err)
     }
 }
-
 run()
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Power tools
+> For Aragon Power users
+
+batches common cli transactions
+
+## behaviour
+- [ ] on start up it loads the last DAO if there was one 
+- [ ] asks user to use this dao or new
+  - [x] if new enter the add dao flow
+- [ ] drops into tool selection, displys all 4 tools with information
+  - [ ] drops into tool flow
+  - [ ] confirm submittion
+- [ ] submit transaction
+  
