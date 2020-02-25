d6d3313d2a506b443e4f0fa434165c2c291f53da
pythonpete32 <pythonpete32@gmail.com>
2020-02-25 13:38:59 +0000

    add main menu

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 const Configstore = require('configstore');
-const chalkAnimation = require('chalk-animation');
 
 
 // lib imports
@@ -24,18 +23,12 @@ const banner = () => {
             })
         )
     )
-
     console.log(
         chalk.blue.bold('For Aragon Power Users')
     )
 }
 
 // check for saved dao
-if (daoConf) { // no saved dao
-    console.log(daoConf)
-    setTimeout(() => {
-    }, 1000);
-}
 
 
 // run program
@@ -43,13 +36,31 @@ const run = async () => {
     banner()
 
     try {
+        let loop = true
         const newDAO = await inquirer.login()
         console.log(newDAO)
-        if (newDAO){
+        if (newDAO) {
             await inquirer.askDaoAddresses()
-        }    
-        
-        await inquirer.getMints()
+        }
+        let c = await inquirer.menu()
+        console.log('c: ', c)
+        console.log('returns: ', c.command)
+
+        if(c.command == 'mint'){
+            console.log(await inquirer.getMints());
+        }
+        if(c.command == 'payments'){
+            console.log('paymenffts')
+        }
+        if(c.command == 'swap'){
+            console.log('swap')
+        }
+        if(c.command == 'permissions'){
+            console.log('permissions')
+        }
+        if(c.command == 'quit'){
+            console.log('quit')
+        }
 
     } catch (err) {
         console.log(err)
